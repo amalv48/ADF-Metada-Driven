e959d000e6df7519ce497f413e506d9ff3b55427
Adrian M <a.malvianto@yahoo.com>
2022-03-05 23:18:24 +1100

    Add process and load tables

---
+++ ctl.proces.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS ctl.process;
+GO
+--CREATE SCHEMA ctl;
+GO
+CREATE TABLE ctl.process 
+(
+    process_id INT, 
+    process_name VARCHAR(50),
+    PRIMARY KEY (process_id)
+);
+
+
+INSERT INTO ctl.process
+VALUES 
+    (1, 'Source_To_Incoming'),
+    (2, 'Incoming_To_Sql')
+;
